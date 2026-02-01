94dcfe577fa721d9a19eb2f91b84ac69b7b12f15
ali <12068482+alifa20@users.noreply.github.com>
2026-02-01 15:33:57 +1100

    feat: redux added into lib folder

---
+++ lib/store/index.ts
@@ -0,0 +1,4 @@
+export { makeStore } from './store'
+export type { RootState, AppDispatch } from './store'
+export { useAppDispatch, useAppSelector, useAppStore } from './hooks'
+export { StoreProvider } from './StoreProvider'
